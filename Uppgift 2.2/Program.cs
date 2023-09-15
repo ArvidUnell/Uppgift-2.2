0be4cfe1445cbea2009cf691df04e56ac4868830
arun06001 <arun06001@E-PW0234KE>
2023-09-15 10:09:21 +0200

    Add project files.

---
+++ Uppgift 2.2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Uppgift_2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många meter hoppade Elin?");
+            float ElinHopp = float.Parse(Console.ReadLine());
+            Console.WriteLine("Hur många meter hoppade Alma?");
+            float AlmaHopp = float.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Elin hoppade {Math.Round(ElinHopp - AlmaHopp,2)} meter längre än Alma."); //Avrundar för att operationer med float ibland ger lite fel svar
+
+            Console.ReadKey();
+        }
+    }
+}
